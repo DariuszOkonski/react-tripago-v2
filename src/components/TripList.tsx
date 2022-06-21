a4617f9f5ffb6b327e9c428183f0eff8bb0f0af5
Dariusz Okonski <darek200180@gmail.com>
2022-06-21 08:09:38 +0100

    add filtering

--- src/components/TripList.tsx
+++ src/components/TripList.tsx
@@ -5,16 +5,18 @@ interface TripListModel {
   title: string;
   price: string;
   id: string;
+  loc: string;
 }
 
 const TripList: React.FC = () => {
   const [trips, setTrips] = useState<TripListModel[]>([]);
+  const [url, setUrl] = useState<string>("http://localhost:3000/trips");
 
   useEffect(() => {
-    fetch("http://localhost:3000/trips")
+    fetch(url)
       .then((response) => response.json())
       .then((json) => setTrips(json));
-  }, []);
+  }, [url]);
 
   return (
     <div className="trip-list">
@@ -24,9 +26,25 @@ const TripList: React.FC = () => {
           <li key={trip.id}>
             <h3>{trip.title}</h3>
             <p>{trip.price}</p>
+            <small>location: {trip.loc}</small>
           </li>
         ))}
       </ul>
+      <div className="filters">
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?loc=europe")}
+        >
+          European Trips
+        </button>
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?loc=america")}
+        >
+          American Trips
+        </button>
+        <button onClick={() => setUrl("http://localhost:3000/trips")}>
+          All Trips
+        </button>
+      </div>
     </div>
   );
 };
